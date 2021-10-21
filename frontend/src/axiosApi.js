77325a91ef203f2d172110ce273fc7a6d6f97552
Omar <omardraz94@gmail.com>
2021-10-20 22:16:59 -0700

    small changes

--- frontend/src/axiosApi.js
+++ frontend/src/axiosApi.js
@@ -10,4 +10,33 @@ const axiosInstance = axios.create({
     }
 });
 
-export default axiosInstance;
+axiosInstance.interceptors.response.use(
+    response => response,
+    error => {
+      const originalRequest = error.config;
+
+      if (error.response.status === 401 && error.response.statusText === "Unauthorized") {
+          const refresh_token = localStorage.getItem('refresh_token');
+
+          return axiosInstance
+              .post('/token/refresh/', {refresh: refresh_token})
+              .then((response) => {
+
+                  localStorage.setItem('access_token', response.data.access);
+                  localStorage.setItem('refresh_token', response.data.refresh);
+
+                  axiosInstance.defaults.headers['Authorization'] = "JWT " + response.data.access;
+                  originalRequest.headers['Authorization'] = "JWT " + response.data.access;
+
+                  return axiosInstance(originalRequest);
+              })
+              .catch(err => {
+                  console.log(err)
+              });
+      }
+      // specific error handling done elsewhere
+      return Promise.reject({...error});
+  }
+);
+
+export default axiosInstance
