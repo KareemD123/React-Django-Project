d0d6557910b124f8e429c1c32d85e03c28734dc1
Omar <omardraz94@gmail.com>
2021-10-22 19:49:53 -0700

    finished the tutorial

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,8 +3,30 @@ import { Switch, Route, Link } from "react-router-dom";
 import Login from "./login";
 import Signup from "./signup";
 import Hello from './hello';
+import axiosInstance from "../axiosApi";
 
 class App extends Component {
+        constructor() {
+            super();
+            this.handleLogout = this.handleLogout.bind(this);
+        }
+
+        async handleLogout() {
+        try {
+            const response = await axiosInstance.post('/blacklist/', {
+                "refresh_token": localStorage.getItem("refresh_token")
+            });
+            localStorage.removeItem('access_token');
+            localStorage.removeItem('refresh_token');
+            axiosInstance.defaults.headers['Authorization'] = null;
+            return response;
+        }
+        catch (e) {
+            console.log(e);
+        }
+    };
+
+
     render() {
         return (
             <div className="site">
@@ -13,6 +35,7 @@ class App extends Component {
                     <Link className={"nav-link"} to={"/login/"}>Login</Link>
                     <Link className={"nav-link"} to={"/signup/"}>Signup</Link>
                     <Link className={"nav-link"} to={"/hello/"}>Hello</Link>
+                    <button onClick={this.handleLogout}>Logout</button>
                 </nav>
                 <main>
                     <h1>Ahhh after 10,000 years I'm free. Time to conquer the Earth!</h1>
