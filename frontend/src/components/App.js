cf8a886ef8350454f82cba85f685b740e43dbf7f
Omar <omardraz94@gmail.com>
2021-11-05 19:29:14 -0700

    figured out how to do grid stuff

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -30,23 +30,20 @@ class App extends Component {
 
     render() {
         return (
-            <div className="site">
-                <nav>
+            <div className='navbar'>
+                <div className='header'>
                     <Link className={"nav-link"} to={"/"}>Home</Link>
                     <Link className={"nav-link"} to={"/login/"}>Login</Link>
                     <Link className={"nav-link"} to={"/signup/"}>Signup</Link>
                     <Link className={"nav-link"} to={"/users/"}>List of Users</Link>
                     <button className={"button-align"} onClick={this.handleLogout}>Logout</button>
-                </nav>
-                <main>
-                    <h1>Ahhh after 10,000 years I'm free. Time to conquer the Earth!</h1>
-
                     <Switch>
                         <Route exact path={"/login/"} component={Login}/>
                         <Route exact path={"/signup/"} component={Signup}/>
                         <Route exact path={"/users/"} component={ListofUsers}/>
-                        <Route path={"/"} render={() => <div>Home again</div>}/>
                     </Switch>
+                </div>
+                <main>
                 </main>
             </div>
             
